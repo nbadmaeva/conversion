07e4ebbecf9b43e4324b52ce692e775bd3e9f90a
Natalia <nbadmaeva@outlook.com>
2013-05-07 17:46:00 +0700

    Update fahrenheit_to_kelvin.py

--- fahrenheit_to_kelvin.py
+++ fahrenheit_to_kelvin.py
@@ -1,8 +1,10 @@
+#!/usr/bin/env python
+
 # Conversion Fahrenheit to Kelvin
 # Formula: K = (F - 32)/ 1.8 + 273.15
 
 def fahrenheit_to_kelvin(fahrenheit):
     celsius = 5 / 9 * (fahrenheit - 32)
-    return celsius + 273.15 
-
+    return celsius + 273.15
+    
 print fahrenheit_to_kelvin(1)
